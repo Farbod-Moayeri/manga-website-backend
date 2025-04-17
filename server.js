28fc6d8badc1da907e4f45b15a45c69e526895f9
Farbod Moayeri <fmoayeri2@myseneca.ca>
2025-04-17 16:31:53 -0400

    created routes for /api/manga and /api/manga/:mangaName

--- server.js
+++ server.js
@@ -1,10 +1,71 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path')
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
-.catch((err) => {
-    console.log(err);
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+//app.use('/manga', express.static())
+
+
+app.get('/api/manga', (req,res) => {
+    const dirPath = path.join(__dirname, 'manga');
+    
+    fs.readdir(dirPath, (err, files) => {
+        if (err) {
+            console.log(err);
+
+            return;
+        }
+        
+        res.json(files);
+    }) 
 })
+app.get('/api/manga/:mangaName', (req,res) => {
+    const { mangaName } = req.params
 
-app.use(express.static('manga'))
+    const dirPath = path.join(__dirname, 'manga');
+
+    fs.readdir(dirPath, (err, files) => {
+        if (err) {
+            console.log(err);
+
+            return;
+        }
+        
+        files.forEach((file) => {
+            if (file.toLowerCase() == mangaName.toLowerCase())
+            {
+                const chapterPath = path.join(__dirname, 'manga', file);
+
+                fs.readdir(chapterPath, (err, files) => {
+                    if (err) {
+                        console.log(err);
+            
+                        return;
+                    }
+                    
+                    files = files.sort((a, b) => {
+                        const getNumber = str => parseFloat(str.replace("Episode ", ""));
+                        return getNumber(a) - getNumber(b);
+                    });
+                    
+                    res.statusCode = 200;
+                    res.json(files);
+                })
+
+            }
+        })
+    }) 
+
+    if (res.statusCode != 200) {
+        res.statusCode = 404;
+        res.send('Not Found');
+    }
+
+})
+ app.get('/api/manga/:mangaName/:chapter', (req, res) => {
+     const { mangaName } = req.params
+     const { chapter } = req.params
+});
